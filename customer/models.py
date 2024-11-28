c39753191701e7f6bcec910ecacd45e60526e0f9
nmartinez71 <nmartinez71@ivytech.edu>
2024-11-27 23:10:49 -0500

    models

--- customer/models.py
+++ customer/models.py
@@ -4,3 +4,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     img = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('Category',related_name='item')
+
+    def __str__(self):
+        return self.name
