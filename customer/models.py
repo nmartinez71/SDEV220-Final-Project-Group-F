281978534c1e337f9eec0cc291ff57a3a2251fab
nmartinez71 <nmartinez71@ivytech.edu>
2024-11-29 09:06:37 -0500

    UPDATE

--- customer/models.py
+++ customer/models.py
@@ -9,3 +9,17 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
--- customer/views.py
+++ customer/views.py
@@ -9,3 +9,11 @@ class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
     
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        appetizers = me
+        """#Get every item from each category
+
+        #Pass into context
+
+        #Render template"""
